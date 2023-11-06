14950dbb2e33fc6f83be09d5acb0068c5c203d9a
Aamir <aamirmushtaqengr@gmail.com>
2023-11-06 23:10:18 +0500

    aa

---
+++ out/_next/static/EEBV_N_Ud3NWs56zwMyzl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/12-49a018b9fd574dbd.js","static/chunks/pages/index-0901aeb5e3e85a51.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
