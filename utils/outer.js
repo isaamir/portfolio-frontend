12937ca7df8b82b8c90c9d0cfd49b5c0b00ab7ba
Aamir <aamirmushtaqengr@gmail.com>
2023-11-06 23:05:37 +0500

    change cv

--- utils/outer.js
+++ utils/outer.js
@@ -8,7 +8,7 @@ const outer = {
         'A Software Engineer',
         'I build things for the web',
     ],
-    desciption: `As a frontend web developer With 3 years of experience, I prioritize attention to detail and specialize in creating functional websites using modern frameworks and tools. My work focuses on delivering immersive user experiences with seamless navigation and responsive design. Browse my portfolio for examples of my recent work and contact me to discuss how I can help bring your vision to life. `,
+    desciption: `As a frontend web developer With 4 years of experience, I prioritize attention to detail and specialize in creating functional websites using modern frameworks and tools. My work focuses on delivering immersive user experiences with seamless navigation and responsive design. Browse my portfolio for examples of my recent work and contact me to discuss how I can help bring your vision to life. `,
     button: {
         label: 'Contact me!',
         onClick: () => openLink('mailto:aamirmushtaqengr@gmail.com?subject=Hello')
